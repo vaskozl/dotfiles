8e69b8ee69a5036497ad3b11ac821b9f253e4a40
Vasil Zlatanov <vasil.zlatanov@gmail.com>
2014-08-03 18:19:04 +0300

    fix mutt aliases

--- mutt/alias.sh
+++ mutt/alias.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
